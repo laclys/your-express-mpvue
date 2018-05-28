87f7741e744edc20422aec0af9bbf9f89c461dc8
Lac <laclys@126.com>
2018-05-28 23:00:45 +0800

    add style

--- src/App.vue
+++ src/App.vue
@@ -11,5 +11,16 @@ export default {
 </script>
 
 <style>
-
+  .btn {
+    color: white;
+    background-color: rgba(30, 156, 214, 1);
+    margin-bottom: 10rpx;
+    padding-left: 15rpx;
+    padding-right: 15rpx;
+    border-radius: 5rpx;
+    width: 100%;
+  }
+  .btn:active {
+    background-color: rgba(30, 156, 214, 0.5);
+  }
 </style>
