65fa018e7e4300000cc6ba5da86eae8ae6a9edfd
Lac <laclys@126.com>
2018-06-03 15:48:40 +0800

    added

--- src/pages/express/Express.vue
+++ src/pages/express/Express.vue
@@ -1,25 +1,79 @@
 <template>
   <div class="container">
+    <section>
+      <picker @change="pickerChange" :value="index" :range="companyList" range-key="name">
+        <view class="picker">
+          快递公司：{{companyList[index].name}}
+        </view>
+      </picker>
+    </section>
     <p class="title">单号查起来~:</p>
     <input
       class="input"
       focus="true"
       type="number"
+      v-model="nu"
     />
-    <button class="btn sreach-btn" >Sreach</button>
+    <button class="btn sreach-btn" @click="handleSreach" >Sreach</button>
     <img class="screach-icon" src="../../../static/img/sreach.png" alt="">
   </div>
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      nu: '',
+      index: 0,
+      companyList: [
+        {
+          name: '顺丰',
+          code: 'shunfeng'
+        },
+        {
+          name: '百世',
+          code: 'huitongkuaidi'
+        },
+        {
+          name: '中通',
+          code: 'zhongtong'
+        },
+        {
+          name: '韵达',
+          code: 'yunda'
+        },
+        {
+          name: '申通',
+          code: 'shentong'
+        },
+        {
+          name: '京东',
+          code: 'jd'
+        },
+      ]
+    }
+  },
+  methods: {
+    pickerChange (e) {
+      console.log(e.target.value)
+      this.index = e.target.value
+    },
+    handleSreach () {
+      console.log('快递单号',this.nu)
+      console.log('快递公司代号',this.companyList[this.index].code)
+    }
+  }
 }
 </script>
 
 <style lang="scss">
   .container {
     margin: 100rpx 30rpx;
+    .picker {
+      margin-bottom: 10rpx;
+      font-size: 36rpx;
+      color: #1e9cd6;
+    }
     .title{
       margin-bottom: 30rpx;
       font-size: 36rpx;
